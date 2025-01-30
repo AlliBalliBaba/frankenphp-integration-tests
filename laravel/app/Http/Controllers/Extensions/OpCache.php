b25cedc2395c630c689dbcbc27e10f3185bee16b
a.stecher <a.stecher@sportradar.com>
2025-01-30 21:12:31 +0100

    Adjusts caddy config.

--- laravel/app/Http/Controllers/Extensions/OpCache.php
+++ laravel/app/Http/Controllers/Extensions/OpCache.php
@@ -7,8 +7,8 @@ class OpCache
 
     public function flush(): array
     {
-        // TODO: This breaks non-worker mode
-        opcache_reset();
+        // TODO: This breaks non-worker mode and worker mode while scaling
+        //opcache_reset();
         return ['success' => true];
     }
 
