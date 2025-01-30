63afa193bf42cc7f6596c5e6c8422fda240d43fd
a.stecher <a.stecher@sportradar.com>
2025-01-30 16:27:51 +0100

    Fixes curl timeouts.

--- laravel/tests/Extensions/CurlTest.php
+++ laravel/tests/Extensions/CurlTest.php
@@ -15,7 +15,8 @@ class CurlTest extends FeatureTestCase
     {
         $proxyUrl = urlencode("http://localhost");
 
-        $this->fetchParallelTimes(new TestRequest("/curl?url=$proxyUrl"), 100, function (Response $response) {
+        // 'count' must be less than worker number since we are using the same worker to fetch the proxy
+        $this->fetchParallelTimes(new TestRequest("/curl?url=$proxyUrl"), 10, function (Response $response) {
             $this->assertOk($response);
             $this->assertJsonResponse([
                 'result' => 'Hello World!',
@@ -28,7 +29,7 @@ class CurlTest extends FeatureTestCase
     {
         $proxyUrl = urlencode("http://localhost/sleep?ms=5000");
 
-        $this->fetchParallelTimes(new TestRequest("/curl?timeout=1&url=$proxyUrl"), 100, function (Response $response) {
+        $this->fetchParallelTimes(new TestRequest("/curl?timeout=1&url=$proxyUrl"), 20, function (Response $response) {
             $this->assertJsonResponse([
                 'result' => false
             ], $response);
