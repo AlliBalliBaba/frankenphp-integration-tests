387f975a65756d8ba4f608c6f5d2eb5503d06f47
a.stecher <a.stecher@sportradar.com>
2025-02-01 23:26:45 +0100

    Adds timeout test.

---
+++ laravel/tests/Server/TimeoutTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Server;
+
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\FeatureTestCase;
+use Tests\TestRequest;
+
+class TimeoutTest extends FeatureTestCase
+{
+
+    #[Test]
+    public function test_timeouts()
+    {
+        $this->fetchParallelTimes(new TestRequest("/sleep?ms=20000"), 20, function (Response $response) {
+            $this->assertStatusCode($response, 500);
+        });
+    }
+
+}
